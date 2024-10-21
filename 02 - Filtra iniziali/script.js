94567583a4dd73840aae9a7680f21417ed41b9f5
eduardorocco <eduardo_rocco@hotmail.it>
2024-10-21 16:46:20 +0200

    ADD ex 02

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -5,9 +5,24 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+const letter = "A"
+
+function filter(array, letter) {
+
+    return array.filter(word => word.startsWith(letter))
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+const result = filter(names, letter);
+
+console.log(result)
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+
+
+
+// Esempio di utilizzo
+
+
